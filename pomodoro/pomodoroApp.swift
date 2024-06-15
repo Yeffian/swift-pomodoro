129633a6dd28bc1d5583b5a8da12f0981bf5a836
Yeffian <aditchakraborty2009@gmail.com>
2024-06-15 22:39:07 +0800

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by Adit Chakraborty on 15/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
