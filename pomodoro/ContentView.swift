19320ebee1ce7e2565163a772662107184d78f60
Yeffian <aditchakraborty2009@gmail.com>
2024-06-18 17:39:11 +0800

    Add source files at current stage

--- pomodoro/ContentView.swift
+++ pomodoro/ContentView.swift
@@ -8,14 +8,111 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var restTime = 60
+    @State private var workTime = 90
+    @State private var timeRemaining: TimeInterval = 90
+    @State private var timer: Timer?
+    @State private var isRunning: Bool = false
+    @State private var onBreak: Bool = true
+    @State private var score = 0
+    
+    let formatter: NumberFormatter = {
+         let formatter = NumberFormatter()
+         formatter.numberStyle = .decimal
+         return formatter
+     }()
+    
+    var timerColor: Color {
+        if onBreak {
+            return .cyan
+        } else {
+            return.orange
         }
-        .padding()
+    }
+    
+    var status: String {
+        if onBreak {
+            return "Get some rest!"
+        } else {
+            return "Get working!"
+        }
+    }
+        
+    func GetTimeFmt() -> String {
+        let minutes = Int(timeRemaining) / 60
+        let seconds = Int(timeRemaining) % 60
+        
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func StopTimer() {
+        onBreak.toggle()
+        isRunning = false
+        timer?.invalidate()
+        
+        if onBreak {
+            timeRemaining = TimeInterval(restTime)
+        } else {
+            timeRemaining = TimeInterval(workTime)
+        }
+    }
+    
+    func StartTimer() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            } else {
+                StopTimer()
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .center) {
+                TextField("I want to work for", value: $workTime, formatter: formatter)
+                Text(status)
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                
+                ZStack {
+                    Circle()
+                        .stroke(lineWidth: 20)
+                        .opacity(0.3)
+                    Circle()
+                        .trim(from: 0, to: CGFloat(1 - timeRemaining / Double(workTime)))
+                        .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
+                        .foregroundStyle(timerColor)
+                        .rotationEffect(.degrees(-90))
+                    
+                    Text(GetTimeFmt())
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                    
+                }
+                .padding()
+                .frame(maxWidth: 500)
+                
+                HStack {
+                    Button(isRunning ? "Stop" : "Start") {
+                        isRunning.toggle()
+                        timeRemaining = TimeInterval(workTime)
+                        
+                        if isRunning {
+                            StartTimer()
+                        } else {
+                            StopTimer()
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .buttonBorderShape(.capsule)
+                    .frame(width: 100, height: 100)
+                    .font(.title)
+                }
+            }
+            .padding(.horizontal, 40)
+            .navigationTitle("Pomodoro Timer")
+        }
     }
 }
 
